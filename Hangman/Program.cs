e7aa1631368515bea0ddbd5713a567356e973231
George Yoku Ocran <gyocran@live.com>
2019-11-29 19:48:52 +0000

    Added solution files

---
+++ Hangman/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var word = "word";
+            var arr = word.ToCharArray();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                var ch = arr[i];
+                if (ch == 'o')
+                    arr[i] = '_';
+                //Console.Write(arr[i] + " ");
+            }
+            Console.Write(arr.ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Hangman/WordDictionary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    public struct Word
+    {
+        public string Name { get; }
+        public string Meaning { get; set; }
+
+        public Word(string name, string meaning)
+        {
+            this.Name = name;
+            this.Meaning = meaning;
+        }
+    }
+
+    public class WordDictionary
+    {
+        // Words sourced from dictionary.com
+        private ArrayList Words = new ArrayList
+        {
+            new Word("corybantic", "frenzied; agitated; unrestrained."),
+            new Word("foison", "abundance; plenty.")
+        };
+
+        //private static Word SelectRandomWord()
+        //{
+            
+        //}
+    }
+}
